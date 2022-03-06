62aab9589c643728aa7ffa6989d4aa2931e01c21
Ivowainer <ivancamposwainer@gmail.com>
2022-03-06 17:44:23 -0300

    Finish

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">  <!-- d-flex vh-100 align-items-center justify-content-center flex-column -->
+    <div class="container d-flex vh-100 align-items-center justify-content-center flex-column">
             <!-- Mensajes de alerta -->
             <?php 
                 if(isset($_GET['message']) and $_GET['message'] === 'success'){
